6340d66e45ac506c76ef5b0013b1fc1895762f44
Thy Khuu <62774638+bachthyaglx@users.noreply.github.com>
2022-10-26 18:02:55 +0300

    Update applicationController.js

--- drill-and-practice/routes/controllers/applicationController.js
+++ drill-and-practice/routes/controllers/applicationController.js
@@ -1,34 +1,36 @@
-import * as userService from "../../services/userService.js";
+import * as applicationService from "../../services/applicationService.js";
 import { validasaur } from "../../deps.js";
-import { bcrypt } from "../../deps.js";
 
 const validationRules_application = {
   date_rent: [validasaur.required, validasaur.isDate],
   type_apartment: [validasaur.required, validasaur.minLength(4)],
 };
 
-const submitApplication = async ({ request, response, render }) => {
-  const body = request.body({ type: "form" });
-  const params = await body.value;
+const submitApplication = async ({ request, response, render, user }) => {
+  const apartmentID = await applicationService.getApartmentID(form_value.type_apartment);
+  console.log(apartmentID);
+  // const body = request.body({ type: "form" });
+  // const form_value = await body.value;
 
-  const apartmentData = {
-    date_rent: params.get("date_rent"),
-    type_apartment: params.get("type_apartment"),
-  };
+  // const apartmentData = {
+  //   date_rent: form_value.get("date_rent"),
+  //   type_apartment: form_value.get("type_apartment"),
+  // };
 
-  const [passes, errors] = await validasaur.validate(userData, validationRules_application);
+  // const [passes, errors] = await validasaur.validate(apartmentData, validationRules_application);
 
-  if (!passes) {
-    userData.regErrors = errors;
-    render("application.eta", userData);
-  } else {
-    await userService.addUser(params.get("firstname"), params.get("lastname"), params.get("birthdate"), params.get("gender"), params.get("email"), await bcrypt.hash(params.get("password")));
-    response.redirect("/auth/login");
-  }
+  // if (!passes) {
+  //   userData.regErrors = errors;
+  //   render("application.eta", apartmentData);
+  // } else {
+  //   const apartmentID = await applicationService.getApartmentID(form_value.type_apartment);
+  //   await applicationService.submitApplication(user.id, apartmentID, CURRENT_DATE, form_value.date_rent);
+  //   response.redirect("/auth/login");
+  // }
 };
 
-const showApplication= ({ render }) => {
+const showApplicationForm = ({ render }) => {
   render("application.eta");
 };
 
-export { submitApplication, showApplication };
+export { submitApplication, showApplicationForm };
