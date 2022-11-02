3bfaf0fc0a10c6ff68998edf7f280c4cd83a1788
Thy Khuu <62774638+bachthyaglx@users.noreply.github.com>
2022-11-02 19:23:43 +0200

    Add files via upload

--- app/services/admin_applicationService.js
+++ app/services/admin_applicationService.js
@@ -2,7 +2,7 @@ import { executeQuery } from "../database/database.js";
 
 const displayListUserInfo = async () => {
   const table1 = await executeQuery(
-    `SELECT tr1.firstname, tr1.lastname, tr1.gender, tr1.email, tr2.type, tr2.price, t.date_request, t.date_rent
+    `SELECT tr1.id, tr1.firstname, tr1.lastname, tr1.gender, tr1.email, tr2.type, tr2.price, t.date_request, t.date_rent
         FROM application AS t 
         INNER JOIN users AS tr1 ON t.user_id = tr1.id
         INNER JOIN apartments AS tr2 ON t.apartment_id = tr2.id
@@ -14,7 +14,7 @@ const displayListUserInfo = async () => {
 
 const displayListRoom = async (firstname, sex) => {
   const table2 = await executeQuery(
-    `SELECT city, zipcode, address FROM rooms AS t WHERE apartment_id IN (SELECT apartment_id FROM application WHERE user_id IN (SELECT id FROM users AS c WHERE firstname=$firstname)) AND (t.sex='' OR t.sex=$sex)`,
+    `SELECT id, city, zipcode, address FROM rooms AS t WHERE apartment_id IN (SELECT apartment_id FROM application WHERE user_id IN (SELECT id FROM users AS c WHERE firstname=$firstname)) AND (t.sex='' OR t.sex=$sex)`,
     {
       firstname: firstname,
       sex: sex,
